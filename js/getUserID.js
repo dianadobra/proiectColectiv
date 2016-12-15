47e727d712c6f3eb81e84c126751ef642ac66ade
Arty <Arty>
2016-12-15 19:49:46 +0200

    Delete done + download wip

---
+++ js/getUserID.js
@@ -0,0 +1,10 @@
+function getID(parse_id) {
+  var obj = JSON.parse(parse_id);
+  // var i = 7;
+  // var authorId = obj[6];
+  // while(obj[i] != ',') {
+  //   authorId = authorId + obj[i];
+  //   i++;
+  // }
+  var index = user.search("id");
+}
